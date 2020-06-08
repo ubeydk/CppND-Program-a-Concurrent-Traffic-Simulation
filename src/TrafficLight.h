741f69ad1746b1d0f77799719c0fe407c2f99ad1
Ubeydullah Kuluk <kulukubeyd@gmail.comh>
2020-06-07 21:52:13 -0700

    implement task 1, define functions within Trafficight.h

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -30,7 +30,13 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+enum TrafficLightPhase
+{
+    red,
+    green
+};
+
+class TrafficLight : TrafficObject
 {
 public:
     // constructor / desctructor
@@ -38,14 +44,17 @@ public:
     // getters / setters
 
     // typical behaviour methods
-
+    void waitForGreen();
+    void simulate();
+    TrafficLightPhase getCurrentPhase();
 private:
     // typical behaviour methods
-
+    void cycleThroughPhases();
+    
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
-
+    TrafficLightPhase _currentphase;
     std::condition_variable _condition;
     std::mutex _mutex;
 };
